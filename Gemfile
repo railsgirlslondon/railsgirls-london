47ffb4bf5ebef1de7d61db6685a90fbe71b6596b
Damon Davison <damon@allolex.net>
2013-03-24 09:46:27 +0000

    Kick out the jams!

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+
+group :test do
+  gem 'rspec-rails'
+  gem 'capybara-webkit'
+  gem 'shoulda-matchers'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsgirlsLondon::Application.config.secret_token = '35eb036d7fc0b9c99bfa90c0475be23f4786ea40014a4b111be4333e1c977c8bc32f2a1f806c81d34c5016b32bad611e4d4c1b66b2f01076e8463bee0244a98f'
