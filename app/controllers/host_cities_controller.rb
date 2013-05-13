d47d46ee7134ebae7a99e91e1ef1fdfa5ac912e6
Despo Pentara <despo@extractmethod.com>
2013-05-13 17:25:08 +0100

    remove unused city layout
    rename host_city layout

--- app/controllers/host_cities_controller.rb
+++ app/controllers/host_cities_controller.rb
@@ -4,7 +4,7 @@ class HostCitiesController < ApplicationController
     @host_city = HostCity.find_by_slug(params[:slug])
 
     respond_to do |format|
-      format.html { render layout: 'layouts/host_city_layout' }
+      format.html { render layout: 'layouts/host_city' }
       format.json { render json: @host_city }
     end
   end
