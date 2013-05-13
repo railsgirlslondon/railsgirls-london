1e412382ea52bf409ef263b670d9f858a11a80b8
Despo Pentara <despo@extractmethod.com>
2013-05-13 16:26:23 +0100

    remove index from host_cities_controller

--- app/controllers/host_cities_controller.rb
+++ app/controllers/host_cities_controller.rb
@@ -1,14 +1,5 @@
 class HostCitiesController < ApplicationController
 
-  def index
-    @host_cities = HostCity.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @host_cities }
-    end
-  end
-
   def show
     @host_city = HostCity.find_by_slug(params[:slug])
 
