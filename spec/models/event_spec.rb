1a03924d1a2ccd77d8f21d4a1e57ed7389f8c6c7
Despo Pentara <despo@extractmethod.com>
2013-05-14 13:02:47 +0100

    display even dates correctly

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
-describe Event, wip: true do
+describe Event do
   it { should belong_to(:host_city) }
 
   it { should validate_presence_of(:start_date) }
   it { should validate_presence_of(:end_date) }
+
+  context "#display dates" do
+    it 'an event starting and finishing in the same month' do
+      event = FactoryGirl.create(:event)
+
+      event.dates.should eq("19-20th April")
+    end
+
+  end
 end
