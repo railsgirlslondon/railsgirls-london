3a8e30718cb5560926d8db4ab9dd1ead185ea6ee
Despo Pentara <despo@extractmethod.com>
2013-05-24 00:50:53 +0100

    add slug to city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,10 @@
 class City < ActiveRecord::Base
   attr_accessible :name, :twitter
+  attr_protected :slug
 
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523212928) do
+ActiveRecord::Schema.define(:version => 20130523234411) do
 
   create_table "cities", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "twitter"
+    t.string   "slug"
   end
 
   create_table "registrations", :force => true do |t|
