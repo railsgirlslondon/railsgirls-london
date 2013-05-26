6277e22f4ac3e920add0dd33da4c30ea3377ac2f
Damon Davison <damon@allolex.net>
2013-05-26 14:32:22 +0100

    Destroy old entries when seeding the database in dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
+if Rails.env.development? || Rails.env.test?
+  City.destroy_all
+end
 City.create name: 'London', twitter: '@railsgirls_ldn'
-
 City.all.each { |c| c.save }
