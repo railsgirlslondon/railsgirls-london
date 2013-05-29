b431d8dec1dfc79690c9b2a0906cbc71304c0e21
Jeff Taggart <jetaggart@gmail.com>
2013-05-29 21:06:11 +0100

    symbolize schema keys

--- db/schema.rb
+++ db/schema.rb
@@ -9,41 +9,41 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130527142342) do
+ActiveRecord::Schema.define(version: 20130527142342) do
 
-  create_table "cities", :force => true do |t|
+  create_table "cities", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "twitter"
     t.string   "slug"
   end
 
-  add_index "cities", ["slug"], :name => "index_cities_on_slug"
+  add_index "cities", ["slug"], name: "index_cities_on_slug"
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: true do |t|
     t.text     "description"
     t.integer  "city_id"
     t.boolean  "active"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.date     "starts_on"
     t.date     "ends_on"
   end
 
-  create_table "registrations", :force => true do |t|
-    t.string   "first_name",             :null => false
-    t.string   "last_name",              :null => false
-    t.string   "email",                  :null => false
+  create_table "registrations", force: true do |t|
+    t.string   "first_name",             null: false
+    t.string   "last_name",              null: false
+    t.string   "email",                  null: false
     t.string   "twitter"
     t.string   "gender"
     t.string   "phone_number"
     t.text     "programming_experience"
     t.text     "reason_for_applying"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "uk_resident"
     t.string   "os"
     t.string   "os_version"
@@ -53,16 +53,16 @@ ActiveRecord::Schema.define(:version => 20130527142342) do
     t.integer  "event_id"
   end
 
-  add_index "registrations", ["email"], :name => "index_registrations_on_email"
-  add_index "registrations", ["last_name"], :name => "index_registrations_on_last_name"
+  add_index "registrations", ["email"], name: "index_registrations_on_email"
+  add_index "registrations", ["last_name"], name: "index_registrations_on_last_name"
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -70,12 +70,12 @@ ActiveRecord::Schema.define(:version => 20130527142342) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
