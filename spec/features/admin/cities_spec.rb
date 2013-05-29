599208e875f8bbe26ff145bc3b0731c1bb07ff37
Jeff Taggart <jetaggart@gmail.com>
2013-05-29 23:12:31 +0100

    Fix cities spec

--- spec/features/admin/cities_spec.rb
+++ spec/features/admin/cities_spec.rb
@@ -4,21 +4,22 @@ feature "an admin CRUDing cities" do
   Given { admin_logged_in! }
 
   context "creating cities" do
+    Given { click_on "Cities" }
+    
     When do
-      expect { find(".cities a") }.to raise_error
-
+      expect(page).to_not have_content "Some City"
       click_link "New city"
 
-      fill_in "Name", with: "London"
+      fill_in "Name", with: "Some City"
       click_on "Create City"
     end
 
     Then do
-      expect(page).to have_content("London")
+      expect(page).to have_content("Some City")
 
       visit root_path
 
-      find(".cities a").should have_text "London"
+      find(".cities a").should have_text "Some City"
     end
   end
 end
