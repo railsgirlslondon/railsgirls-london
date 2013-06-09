20851e7cb3b4da3e346fd53cb345e9467bd6d202
Despo Pentara <despo@extractmethod.com>
2013-06-09 18:32:21 +0100

    raise routing error if city doesn't exist

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,12 @@
 class CitiesController < ApplicationController
   def show
     @city = City.find_by_slug(params[:id])
+    not_found unless @city
+  end
+
+  private
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
   end
 end
