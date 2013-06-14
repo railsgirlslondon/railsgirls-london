2aca92fb936eb1ec8345899a993a143b96c9b606
Despo Pentara <despo@extractmethod.com>
2013-06-14 16:20:35 +0100

    add rejection email, content needs to be updated

--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -29,4 +29,18 @@ describe RegistrationMailer do
     ActionMailer::Base.deliveries.last.body.encoded.should include event.dates
   end
 
+  it "sends an email when an applicant is rejected" do
+    event = Fabricate(:event)
+    registration = Fabricate(:registration)
+
+    subject= "Regarding Rails Girls #{event.city_name} (#{event.dates})"
+
+    RegistrationMailer.application_rejected(event, registration).deliver
+
+    ActionMailer::Base.deliveries.last.From.to_s.should == "Rails Girls #{event.city.name} <#{event.city.email}>"
+    ActionMailer::Base.deliveries.last.To.to_s.should == registration.email
+    ActionMailer::Base.deliveries.last.subject.should eq subject
+    ActionMailer::Base.deliveries.last.body.encoded.should include registration.first_name
+    ActionMailer::Base.deliveries.last.body.encoded.should include event.dates
+  end
 end
