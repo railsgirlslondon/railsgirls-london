c84028027cae58c5e95174b87605d6807918b3ce
Despo Pentara <despo@extractmethod.com>
2013-06-30 20:02:51 +0100

    add image to event and display on city page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130626134402) do
+ActiveRecord::Schema.define(version: 20130630173642) do
 
   create_table "cities", force: true do |t|
     t.string   "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20130626134402) do
     t.date     "registration_deadline"
     t.string   "title"
     t.boolean  "coachable"
+    t.string   "image"
   end
 
   create_table "meeting_types", force: true do |t|
