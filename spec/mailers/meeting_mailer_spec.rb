17afeb5f5dbd42ca505198f5f827289174449eb2
Jeff Taggart <jeff@jetaggart.com>
2013-08-18 13:02:31 +0100

    Add unsubscribe link to email

--- spec/mailers/meeting_mailer_spec.rb
+++ spec/mailers/meeting_mailer_spec.rb
@@ -10,6 +10,7 @@ describe MeetingMailer do
     @email_subject = "Rails Girls #{meeting.city_name} - You are invited to Weeklies (#{meeting.date.to_formatted_s(:long_ordinal)})"
 
     MeetingMailer.invite(meeting, invitation.member, invitation).deliver
+    expect(html_body).to include(unsubscribe_url(invitation.member.uuid))
   end
 
   it "sends an invitation attendance confirmation email" do
@@ -22,8 +23,12 @@ describe MeetingMailer do
     ActionMailer::Base.deliveries.last.subject.should eq @email_subject
     ActionMailer::Base.deliveries.last.From.to_s.should == "Rails Girls #{meeting.city.name} <#{meeting.city.email}>"
     ActionMailer::Base.deliveries.last.To.to_s.should == invitation.member.email
-    ActionMailer::Base.deliveries.last.body.encoded.should include invitation.member.first_name
-    ActionMailer::Base.deliveries.last.body.encoded.should include invitation.member.first_name
-    ActionMailer::Base.deliveries.last.body.encoded.should include invitation_url(invitation)
+    expect(html_body).to include(invitation.member.first_name)
+    expect(html_body).to include(invitation.member.first_name)
+    expect(html_body).to include(invitation_url(invitation))
+  end
+
+  def html_body
+    ActionMailer::Base.deliveries.last.body.parts.first.body
   end
 end
