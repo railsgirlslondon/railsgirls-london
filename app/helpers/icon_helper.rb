633391b7dcf2d0bfdb868d9e650f0f44d9e8c420
Lee Machin <lee@new-bamboo.co.uk>
2013-08-25 13:16:55 +0100

    Use table for coach list, and glyphicons for buttons

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,11 @@
+module IconHelper
+
+  def delete_icon
+    %(<i class="icon-trash icon-white">Delete</i>).html_safe
+  end
+
+  def edit_icon
+    %(<i class="icon-edit">Edit</i>).html_safe
+  end
+
+end
