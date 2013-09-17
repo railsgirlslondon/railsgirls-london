2afc86c4d32464e8bfa9f6e8bb2a9a3014d78b17
Despo Pentara <despo@extractmethod.com>
2013-09-18 00:19:58 +0100

    minimum effort required to get correct twitter feed up for a city

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917215040) do
+ActiveRecord::Schema.define(version: 20130917230609) do
 
   create_table "cities", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20130917215040) do
     t.string   "slug"
     t.string   "email"
     t.string   "coaches_mailing_list"
+    t.string   "twitter_widget_id"
   end
 
   add_index "cities", ["slug"], name: "index_cities_on_slug", using: :btree
