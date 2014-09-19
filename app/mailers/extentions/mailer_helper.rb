d7e23fb33689354d235f8fd9b709d6f3261fcddd
Maria Khait <chiska86@aol.com>
2014-09-19 07:47:25 +0100

    added sponsors to the email

--- app/mailers/extentions/mailer_helper.rb
+++ app/mailers/extentions/mailer_helper.rb
@@ -12,7 +12,7 @@ module Extentions
     end
 
     def load_attachments
-      %w{london-girl.png railsgirls-heart.png  railsgirls-london.png twitter.png github.png facebook.png gplus.png skype.jpg yammer.jpg microsoft.jpg}.each do |image|
+      %w{london-girl.png railsgirls-heart.png  railsgirls-london.png twitter.png github.png facebook.png gplus.png skype.jpg yammer.jpg microsoft.jpg github_logo.png square_enix.png}.each do |image|
         attachments.inline[image] = File.read("#{Rails.root.to_s}/app/assets/images/#{image}")
       end
     end
