bf4428e00145282cbb475961cd2e79d01bc5994b
Maria Khait <chiska86@aol.com>
2015-05-04 23:20:55 +0100

    add secret key to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -215,6 +215,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
 
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
