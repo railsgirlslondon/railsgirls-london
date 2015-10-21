67daee211bc81eee3a5f3956c1ad9b1eaa0dfea0
Raul Gracia <raulgracialario@gmail.com>
2015-10-21 13:24:09 +0100

    Fixing the google map for event details

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -1,33 +1,32 @@
-$(document).ready(function() {
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(0, 0);
+  var mapOptions = {
+    zoom: 12,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  codeAddress();
+}
 
-  var map;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(0, 0);
-    var mapOptions = {
-      zoom: 15,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+function codeAddress() {
+  var address = $('#address').val();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    console.log(results);
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
     }
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    codeAddress();
-  }
+  });
+}
 
-  function codeAddress() {
-    var address = postcode;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
-
+$(document).ready(function() {
+  initialize();
 });
