33feceed274cbc2eb5d650368023b5a82c47bb4d
Maria Khait <chiska86@aol.com>
2015-11-12 15:50:16 +0000

    remove logo

--- app/mailers/extentions/mailer_helper.rb
+++ app/mailers/extentions/mailer_helper.rb
@@ -29,8 +29,7 @@ module Extentions
         "twitter.png",
         "facebook.png",
         "github.png",
-        "gplus.png",
-        "makers_academy.png"
+        "gplus.png"
       ].each do |image|
         attachments.inline[image] = File.read("#{Rails.root.to_s}/app/assets/images/#{image}")
       end
