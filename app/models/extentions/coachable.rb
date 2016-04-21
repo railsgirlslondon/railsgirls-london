93fbbfd382c29a3f5152fc31682de7b68cd0dfee
Tony Marklove <tony@fluxbit.co.uk>
2016-04-21 17:42:30 +0100

    Allow `coachings.attended` to be `null` to support old events

--- app/models/extentions/coachable.rb
+++ app/models/extentions/coachable.rb
@@ -11,7 +11,11 @@ module Extentions
       has_many :coachings, as: :coachable
       has_many :coaches, through: :coachings do
         def who_attended
-          where("coachings.attended" => [Coaching.attendeds[:unknown], Coaching.attendeds[:attended]])
+          where("coachings.attended" => [
+            nil,
+            Coaching.attendeds[:unknown],
+            Coaching.attendeds[:attended]
+          ])
         end
       end
 
