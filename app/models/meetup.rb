92d023b031ea0d479cbf3be607983bc354ed0d18
Maria Khait <mariakhait@gmail.com>
2017-10-02 11:08:13 +0100

    Loads of front end changes

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,6 +2,7 @@ class Meetup < ActiveRecord::Base
   include Extentions::Sponsorable
 
   default_scope { order('created_at DESC') }
+  scope :upcoming, -> { where("starts_on >= ?", Date.today) }
 
   def dates
     format_date(starts_on)
