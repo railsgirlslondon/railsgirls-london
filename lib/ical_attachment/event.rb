04664b6e7c9db2edc464666a253d503fedeed1ca
Maria Khait <mariakhait@gmail.com>
2022-10-02 07:59:27 +0100

    It now creates 3 separate ICAL events, this is a load from gmail fix
    Previously was only loading one event through gmail, now it loads all 3

--- lib/ical_attachment/event.rb
+++ lib/ical_attachment/event.rb
@@ -3,6 +3,8 @@ module IcalAttachment
 
     alias_method :event, :model
 
+    TYPES_OF_EVENTS = [:friday_installation_party, :saturday_workshop, :saturday_afterparty]
+
     FRIDAY_START_TIME = '18:30'
     FRIDAY_END_TIME = '21:15'
 
@@ -10,12 +12,17 @@ module IcalAttachment
     SATURDAY_END_TIME = '17:00'
 
     # TODO refactor later
-    def to_event
+    def friday_installation_party
       add_event(event.starts_on, FRIDAY_START_TIME, FRIDAY_END_TIME, 'Installation Party', 'Installing Ruby on Rails & preparing for the workshop! Pizza & DJ 🍕🎵')
-      add_event(event.ends_on, SATURDAY_START_TIME, SATURDAY_END_TIME, 'Workshop', 'Workshop day 💻 💎')
-      add_event(event.ends_on, '17:00', '21:00', 'Afterparty', 'Drinks in a local pub (location TBD on the day)')
     end
 
+    def saturday_workshop
+      add_event(event.ends_on, SATURDAY_START_TIME, SATURDAY_END_TIME, 'Workshop', 'Workshop day 💻 💎')
+    end
+
+    def saturday_afterparty
+      add_event(event.ends_on, '17:00', '21:00', 'Afterparty', 'Drinks in a local pub (location TBD on the day)')
+    end
 
     def add_event(day, start_time, end_time, title, description)
       ical_event = Icalendar::Event.new
